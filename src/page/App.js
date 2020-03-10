9f8ff7b737c2e30f7252dc95707bff9df1081649
isaSofia <isasofiamarx@gmail.com>
2020-03-09 21:53:23 -0300

    fix: conttainer e formulario

--- src/page/App.js
+++ src/page/App.js
@@ -11,10 +11,9 @@ function App() {
       <div className="wrapper">
         <div className="d-flex flex-col mt-5 mb-5 m-md-5">
 
-
           {/* SECTION DA ESQUERDA */}
-          <section className="wrapper-text">
-            <h1 className="main-title mb-5">VAGAS</h1>
+          <section className="wrapper-text m-0 mt-md-3 mb-md-4">
+            <h1 className="main-title mb-4 mb-md-5">VAGAS</h1>
             <h2 className="title mt-4 mr-3 mr-lg-5 mb-md-5">CONSULTOR DE NEGÓCIOS - VENDAS PUBLICIDADE</h2>
             <h3 className="subtitle mt-4 mt-lg-5">Requisitos</h3>
             <p>
@@ -38,66 +37,64 @@ function App() {
 
             <div className="set-margin">
               {/* FORMULÁRIO */}
-              <div className="wrapper-form p-2 mr-3 mr-md-5 mt-md-5">
+              <div className="wrapper-form p-4 mr-0 mr-md-3 mr-md-5 mt-md-5">
                 <div className="content-form p-3 content-fluid d-flex flex-column">
                   <form>
                     <p className="title-form">Envie seu currículo</p>
                     <label className="mb-0">
                       Nome:
                     </label>
-                    <input className="mb-2 m-0 w-100" type="text" name="name" />
+                    <input className="mb-3 m-0 w-100" type="text" name="name" />
                     <label className="mb-0">
                       E-mail:
                     </label>
-                    <input className="mb-2 m-0 w-100" type="email" name="email" />
+                    <input className="mb-3 m-0 w-100" type="email" name="email" />
                     <label className="mb-0">
                       CEP:
                     </label>
-                    <input className="mb-2 m-0 w-100" type="text" name="cep" />
+                    <input className="mb-3 m-0 w-100" type="text" name="cep" />
                     <label className="mb-0">
                       Endereço:
                     </label>
-                    <input className="mb-2 m-0 w-100" type="text" name="endereco" />
+                    <input className="mb-3 m-0 w-100" type="text" name="endereco" />
 
-
-                    {/* PRIMEIRA LINHA COM OS INPUTS LADO A LADO */}
-                    <div className="d-flex flex-row">
-                      <div className="d-flex flex-column w-50">
-                        <label>
+                    <div className="d-flex flex-column flex-md-row">
+                      <div className="w-100 w-md-50 mr-0 mr-md-4">
+                        <label className="mb-0">
                           Número:
                         </label>
-                        <input className="mb-2 m-0" type="text" name="numero" />
+                        <input className="mb-3 m-0 w-100" type="text" name="cep" />
                       </div>
-                      <div className="d-flex flex-column w-50 pl-4">
-                        <label>
+                      
+                      <div className="w-100 w-md-50">
+                        <label className="mb-0">
                           Bairro:
                         </label>
-                        <input className="mb-2 m-0" type="text" name="bairro" />
+                        <input className="mb-3 m-0 w-100" type="text" name="cep" />
                       </div>
                     </div>
-                    {/* /PRIMEIRA LINHA COM OS INPUTS */}
-
-                    {/* SEGUNDA LINHA COM OS INPUTS */}
-                    <div className="d-flex flex-row">
-                      <div className="d-flex flex-column w-50">
-                        <label>
+                    <div className="d-flex flex-column flex-md-row">
+                      <div className="w-100 w-md-50 mr-0 mr-md-4">
+                        <label className="mb-0">
                           Cidade:
                         </label>
-                        <input className="mb-2 m-0" type="text" name="ciidade" />
+                        <input className="mb-3 m-0 w-100" type="text" name="cep" />
                       </div>
-                      <div className="d-flex flex-column w-50 pl-4">
-                        <label>
+                      
+                      <div className="w-100 w-md-50 mb-3 mb-md-0">
+                        <label className="mb-0">
                           Estado:
                         </label>
-                        <input className="mb-2 m-0" type="text" name="estado" />
+                        <input className="mb-3 m-0 w-100" type="text" name="cep" />
                       </div>
                     </div>
-                    {/* /SEGUNDA LINHA COM OS INPUTS */}
+
+                    
 
                     {/* ANEXAR CURRÌCULO */}
-                    <spam className="mt-3 mt-md-5 title-btn">CURRÍCULO</spam>
+                    <spam className="mt-3 mt-md-4 title-btn">CURRÍCULO</spam>
                     <div className="d-flex flex-column flex-md-row mb-4">
-                      <div className="d-flex flex-column">
+                      <div className="d-flex flex-column wrapper-button">
                         <button className="btn btn-all btn-cv mt-2">Anexar currículo</button>
                       </div>
                       <div className="mt-3 mt-md-0 ml-md-4 text-cv d-flex flex-column">
@@ -124,9 +121,9 @@ function App() {
           {/* /SECTION DA ESQUERDA */}
 
           {/* SECTION DA DIREITA */}
-          <section className="wrapper-color">
+          <section className="wrapper-color m-0 mt-md-3 mb-md-4">
             <div className="wrapper-img mt-4">
-              <img className="img-fluid" src={vagas} alt="vagas" />
+              <img className="img-vagas" src={vagas} alt="vagas" />
             </div>
           </section>
           {/* /SECTION DA DIREITA */}
